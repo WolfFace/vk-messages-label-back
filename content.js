f7adbc76625fab5e7b2354b29720f88d1c7eae10
WolfFace <max5.7maxi@gmail.com>
2020-09-29 22:30:12 +0300

    init version

---
+++ content.js
@@ -0,0 +1,46 @@
+const dictionary = {
+  "Messenger": "Messages",
+  "Мессенджер": "Сообщениия"
+}
+
+function correctPageTitle() {
+  const title = document.title;
+  const changeTo = dictionary[title];
+  
+  if (changeTo) {
+    document.title = changeTo;
+  }
+}
+
+window.onload = function() {
+    const titleEl = document.getElementsByTagName("title")[0];
+    const docEl = document.documentElement;
+
+    if (docEl && docEl.addEventListener) {
+        docEl.addEventListener("DOMSubtreeModified", function(evt) {
+            const t = evt.target;
+            if (t === titleEl || (t.parentNode && t.parentNode === titleEl)) {
+                correctPageTitle();
+            }
+        }, false);
+    } else {
+        document.onpropertychange = function() {
+            if (window.event.propertyName == "title") {
+                correctPageTitle();
+            }
+        };
+    }
+};
+
+correctPageTitle();
+
+const elements = document.querySelectorAll(".left_label");
+
+elements.forEach(i => {
+  const label = i.innerHTML;
+  const changeTo = dictionary[label];
+  
+  if (changeTo) {
+    i.innerHTML = changeTo;
+  }
+});
